077123ecaeb689e4bf8951ed7664a9b9209423c2
anastasia-a-zhivaeva <anastasia.a.zhivaeva@gmail.com>
2020-03-01 17:35:40 +0300

    Added sum and binary search functions, Added CicleCI config

--- src/algorithms/algorithms.ts
+++ src/algorithms/algorithms.ts
@@ -1,6 +1,6 @@
 export class Algorithms {
   static binarySearch<T>(array: T[], item: T): number | undefined {
-    if (!array || !array.length) {
+    if (!array?.length) {
       return;
     }
 
@@ -21,7 +21,7 @@ export class Algorithms {
   }
 
   static selectionSort<T>(array: T[]): T[] {
-    if (!array || !array.length) {
+    if (!array?.length) {
       return [];
     }
     const newArray: T[] = [];
@@ -33,7 +33,81 @@ export class Algorithms {
     return newArray;
   }
 
+  static sum(array: number[]): number {
+    if (!array?.length) {
+      return 0;
+    }
+
+    let total = 0;
+    for (const item of array) {
+      total += item;
+    }
+    return total;
+  }
+
+  static sumRecursive(array: number[]): number {
+    if (!array?.length) {
+      return 0;
+    }
+
+    if (array.length === 1) {
+      return array[0];
+    }
+
+    return array[0] + this.sumRecursive(array.slice(1));
+  }
+
+  static elementsCount<T>(array: T[]): number {
+    if (!array?.length) {
+      return 0;
+    }
+
+    return 1 + this.elementsCount(array.slice(1));
+  }
+
+  static max<T>(array: T[]): T | undefined {
+    if (!array?.length) {
+      return;
+    }
+
+    if (array.length === 1) {
+      return array[0];
+    }
+
+    const max = this.max(array.slice(1))!;
+    const current = array[0];
+    return current > max ? current : max;
+  }
+
+  static binarySearchRecursive<T>(
+    array: T[],
+    item: T,
+    low = 0,
+    high = array.length - 1
+  ): number | undefined {
+    if (!array?.length) {
+      return;
+    }
+
+    if (high >= low) {
+      const mid = Math.round((low + high) / 2);
+      const guess = array[mid];
+      if (guess === item) {
+        return mid;
+      }
+      if (guess > item) {
+        return this.binarySearchRecursive(array, item, low, mid - 1);
+      }
+
+      return this.binarySearchRecursive(array, item, mid + 1, high);
+    }
+  }
+
   private static findSmallestIndex<T>(array: T[]): number {
+    if (!array?.length) {
+      return -1;
+    }
+
     let smallest = array[0];
     let smallestIndex = 0;
     array.forEach((item, index) => {
